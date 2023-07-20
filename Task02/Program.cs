16c027038a14a0488cf77a63771cf2bda997fc7a
Sheveliow Igor <strelanedura@list.ru>
2023-07-20 12:38:51 +0300

    Added rest part seminar

---
+++ Task02/Program.cs
@@ -0,0 +1,11 @@
+﻿// // 3. Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+Console.WriteLine("Введите номер дня недели");
+int day = Convert.ToInt32(Console.ReadLine());
+
+
+
+Console.WriteLine("Hello, World!");
---
+++ Task04/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает последнюю цифру
+// этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.WriteLine("Введите трёхзначное целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number < 0)
+{
+    number *= -1;
+}
+if(number >= 100 && number <= 999)
+{
+    int result = number % 10;
+    Console.WriteLine($"Последняя цифра числа: {result}");
+}
+else
+{
+    Console.WriteLine("Число не трёхзначное");
+}
+
+int result2 = number / 100; // первая цифра трёхзначного числа
+
+int result3 = number / 10 % 10; // вторая цифра трёхзначного числа
